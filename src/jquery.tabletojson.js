4c1ec681a56f60a4df4ee1a6abebff09861804e8
lfneves <lfneves.dg@gmail.com>
2014-06-18 20:07:22 +0000

    Test03

--- src/jquery.tabletojson.js
+++ src/jquery.tabletojson.js
@@ -19,26 +19,25 @@
   $.fn.tableToJSON = function(opts) {
 
 	// Set options
-  var defaults = {
-    ignoreColumns: [],
-    onlyColumns: null,
-    ignoreHiddenRows: true,
-    headings: null,
-    allowHTML: false,
-    formatHeader: false,
-    allValueTag: false
-  };
+    var defaults = {
+      ignoreColumns: [],
+      onlyColumns: null,
+      ignoreHiddenRows: true,
+      headings: null,
+      allowHTML: false,
+      formatHeader: false,
+      allValueTag: false
+    };
     opts = $.extend(defaults, opts);
     var notNull = function(value) {
       return value !== undefined && value !== null;
     };
-    
-	var ignoredColumn = function(index) {
-    if( notNull(opts.onlyColumns) ) {
-      return $.inArray(index, opts.onlyColumns) === -1;
-    }
-    return $.inArray(index, opts.ignoreColumns) !== -1;
-  };
+    var ignoredColumn = function(index) {
+      if( notNull(opts.onlyColumns) ) {
+        return $.inArray(index, opts.onlyColumns) === -1;
+      }
+      return $.inArray(index, opts.ignoreColumns) !== -1;
+    };
     
     var arraysToHash = function(keys, values) {
       var keysFormat = [];
@@ -67,42 +66,41 @@
     var override = $(cell).data('override');
     if ( opts.allowHTML ) {
       value = $.trim($(cell).html());
-    } else {
-      if(opts.allValueTag) {
-        var select = $(cell).find('select option:selected').val();		  
-				if($.trim($(cell).text()) === '' || select !== undefined) {
-					var serialized = $(cell).find('input, textarea, select').serialize();
-					var item = $.toDictionary(serialized);
-					value = item;
-				} else {
-					value = $.trim($(cell).text());
-				}
-			} else {
-				value = $.trim($(cell).text());
-			}
-		}
-		result = notNull(override) ? override : value;
-		return result;
-	};
-	
-	var replace = function(firstRow) {
-    var result;
-		if(jQuery.type(firstRow) === 'string') {
-		  result =  firstRow.replace(/[áàâãª]/g, 'a').replace(/[ÁÀÂÃ]/g, 'A')
-			  .replace(/[éèêë]/g, 'e').replace(/[ÉÈÊË]/g, 'E').replace(/[íìî]/g, 'i').replace(/[ÍÌÎ]/g, 'I')
-			  .replace(/[ÓÒÔÕ]/g, 'O').replace(/[óòôõº]/g, 'o').replace(/[úùû]/g, 'u')
-			  .replace(/[ÚÙÛÜ]/g, 'U').replace(/[ç]/g, 'c').replace(/[Ç]/g, 'C').replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '');
-		}
-		return result;
-	};
-
-	var rowValues = function(row) {
-	  var result = [];
-	  $(row).children('td,th').each(function(cellIndex, cell) {
-		result.push( cellValues(cellIndex, cell) );
-	  });
-	  return result;
-	};
+      } else {
+        if(opts.allValueTag) {
+          var select = $(cell).find('select option:selected').val();		  
+				  if($.trim($(cell).text()) === '' || select !== undefined) {
+					  var serialized = $(cell).find('input, textarea, select').serialize();
+					  var item = $.toDictionary(serialized);
+					  value = item;
+				  } else {
+					  value = $.trim($(cell).text());
+				  }
+			  } else {
+				  value = $.trim($(cell).text());
+			  }
+		  }
+    result = notNull(override) ? override : value;
+    return result;
+  };
+    
+    var replace = function(firstRow) {
+      var result;
+      if(jQuery.type(firstRow) === 'string') {
+        result =  firstRow.replace(/[áàâãª]/g, 'a').replace(/[ÁÀÂÃ]/g, 'A')
+.replace(/[éèêë]/g, 'e').replace(/[ÉÈÊË]/g, 'E').replace(/[íìî]/g, 'i').replace(/[ÍÌÎ]/g, 'I').replace(/[ÓÒÔÕ]/g, 'O').replace(/[óòôõº]/g, 'o').replace(/[úùû]/g, 'u')
+.replace(/[ÚÙÛÜ]/g, 'U').replace(/[ç]/g, 'c').replace(/[Ç]/g, 'C').replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '');
+      }
+      return result;
+    };
+    
+    var rowValues = function(row) {
+      var result = [];
+      $(row).children('td,th').each(function(cellIndex, cell) {
+        result.push( cellValues(cellIndex, cell) );
+      });
+      return result;
+    };
 
 	var getHeadings = function(table) {
 	  var firstRow = table.find('tr:first').first();
